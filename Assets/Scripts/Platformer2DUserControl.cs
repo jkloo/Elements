e014b8d467fbc8f0d2880a22c0d6769fb8ced4fd
Jeff Kloosterman <kloosterman.jeff@gmail.com>
2014-03-23 21:53:35 -0400

    added warping, created a tree to climb to test warping

--- Assets/Scripts/Platformer2DUserControl.cs
+++ Assets/Scripts/Platformer2DUserControl.cs
@@ -5,20 +5,40 @@ public class Platformer2DUserControl : MonoBehaviour
 {
 	private PlatformerCharacter2D character;
     private bool jump;
-
-
+	private bool warp;
+	private float warpDelay = 1f;
+	private float timeSinceWarp;
+	
 	void Awake()
 	{
 		character = GetComponent<PlatformerCharacter2D>();
+		timeSinceWarp = warpDelay;
 	}
 
     void Update ()
     {
         // Read the jump input in Update so button presses aren't missed.
+		timeSinceWarp += Time.deltaTime;
 #if CROSS_PLATFORM_INPUT
-        if (CrossPlatformInput.GetButtonDown("Jump")) jump = true;
+        if (CrossPlatformInput.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
+		if (CrossPlatformInput.GetButtonDown("Warp") && timeSinceWarp > warpDelay)
+		{
+			warp = true;
+			timeSinceWarp = 0f;
+		}
 #else
-		if (Input.GetButtonDown("Jump")) jump = true;
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
+		if (Input.GetButtonDown("Warp") && timeSinceWarp > warpDelay)
+		{
+			warp = true;
+			timeSinceWarp = 0f;
+		}
 #endif
 
     }
@@ -34,9 +54,10 @@ public class Platformer2DUserControl : MonoBehaviour
 		#endif
 
 		// Pass all parameters to the character control script.
-		character.Move( h, crouch , jump );
+		character.Move( h, crouch, jump, warp );
 
         // Reset the jump input once it has been used.
-	    jump = false;
+		jump = false;
+		warp = false;
 	}
 }
--- Assets/Scripts/PlatformerCharacter2D.cs
+++ Assets/Scripts/PlatformerCharacter2D.cs
@@ -6,6 +6,7 @@ public class PlatformerCharacter2D : MonoBehaviour
 
 	[SerializeField] float maxSpeed = 8f;				// The fastest the player can travel in the x axis.
 	[SerializeField] float jumpForce = 600f;			// Amount of force added when the player jumps.	
+	[SerializeField] float warpDist = 0.1f;
 
 	[Range(0, 1)]
 	[SerializeField] float crouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
@@ -18,7 +19,6 @@ public class PlatformerCharacter2D : MonoBehaviour
 	bool grounded = false;								// Whether or not the player is grounded.
 	Transform ceilingCheck;								// A position marking where to check for ceilings
 	float ceilingRadius = .01f;							// Radius of the overlap circle to determine if the player can stand up
-	//Animator anim;										// Reference to the player's animator component.
 
 
     void Awake()
@@ -26,7 +26,6 @@ public class PlatformerCharacter2D : MonoBehaviour
 		// Setting up references.
 		groundCheck = transform.Find("GroundCheck");
 		ceilingCheck = transform.Find("CeilingCheck");
-	//	anim = GetComponent<Animator>();
 	}
 
 
@@ -34,54 +33,45 @@ public class PlatformerCharacter2D : MonoBehaviour
 	{
 		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
 		grounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
-	//	anim.SetBool("Ground", grounded);
-
-		// Set the vertical animation
-	//	anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
 	}
 
 
-	public void Move(float move, bool crouch, bool jump)
+	public void Move(float move, bool crouch, bool jump, bool warp)
 	{
 
 
 		// If crouching, check to see if the character can stand up
-	//	if(!crouch && anim.GetBool("Crouch"))
-	//	{
+		if(!crouch)
+		{
 			// If the character has a ceiling preventing them from standing up, keep them crouching
-	//		if( Physics2D.OverlapCircle(ceilingCheck.position, ceilingRadius, whatIsGround))
-	//			crouch = true;
-	//	}
-
-		// Set whether or not the character is crouching in the animator
-	//	anim.SetBool("Crouch", crouch);
-
+			if( Physics2D.OverlapCircle(ceilingCheck.position, ceilingRadius, whatIsGround))
+				crouch = true;
+		}
+		if (warp)
+		{
+			float warpedX = transform.position.x + (facingRight ? warpDist : -warpDist);
+			transform.position = new Vector2(warpedX, transform.position.y);
+		}
 		//only control the player if grounded or airControl is turned on
 		if(grounded || airControl)
 		{
 			// Reduce the speed if crouching by the crouchSpeed multiplier
 			move = (crouch ? move * crouchSpeed : move);
 
-			// The Speed animator parameter is set to the absolute value of the horizontal input.
-	//		anim.SetFloat("Speed", Mathf.Abs(move));
-
 			// Move the character
 			rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
 			
 			// If the input is moving the player right and the player is facing left...
-			if(move > 0 && !facingRight)
-				// ... flip the player.
+			if((move > 0 && !facingRight) || (move < 0 && facingRight))
+			{
 				Flip();
-			// Otherwise if the input is moving the player left and the player is facing right...
-			else if(move < 0 && facingRight)
-				// ... flip the player.
-				Flip();
+			}
 		}
 
         // If the player should jump...
-        if (grounded && jump) {
+        if (grounded && jump)
+		{
             // Add a vertical force to the player.
-      //      anim.SetBool("Ground", false);
             rigidbody2D.AddForce(new Vector2(0f, jumpForce));
         }
 	}
